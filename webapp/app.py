c7f950686f79b5d690473882eb31410aece4b691
cedgerbaud <cgerbaud87@gmail.com>
2022-07-01 14:16:39 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world ! This is Cedric\'s Alpine container speaking'
+    return 'Hello world!'
+
+@app.route('/hello')
+def hello2():
+    return 'This is Cedric\'s Alpine container speaking.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
