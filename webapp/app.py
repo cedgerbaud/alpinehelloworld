1963e787b44b3f8fbb0ac079edc97f270e679169
cedgerbaud <cgerbaud87@gmail.com>
2022-07-01 14:14:13 +0200

    updates app.py to return custom message

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world!'
+    return 'Hello world ! This is Cedric\'s Alpine container speaking'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
